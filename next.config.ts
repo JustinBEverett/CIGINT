0bb21ee9d38ad66d54a767369843d3b967424264
Justin Everett <justin.b.everett@gmail.com>
2026-09-25 12:49:01 -0700

    add cigarette-equivalence calculation and activity cards

--- next.config.ts
+++ next.config.ts
@@ -1,5 +1,7 @@
 import type { NextConfig } from "next";
 
-const nextConfig: NextConfig = {};
+const nextConfig: NextConfig = {
+  allowedDevOrigins: [`process.env.APP_ORIGIN`],
+};
 
 export default nextConfig;
